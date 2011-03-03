4ca8d033530860d9bd33f9857f3ab0a5c9370af3
RobinWu <86695893@qq.com>
2011-03-03 09:54:40 +0800

    change send to sent

--- code/agideo_build_mailer.rb
+++ code/agideo_build_mailer.rb
@@ -5,17 +5,17 @@
 # change
 #  BuildMailer.send(template, build, @emails, from, *args)
 # TO
-#  AgideoBuildMailer.send(template, build, @emails, from, *args)
+#  AgideoBuildMailer.sent(template, build, @emails, from, *args)
 #
 # console test
-# AgideoBuildMailer.send(['***@gmail.com','***@qq.com'], 'test fail')
+# AgideoBuildMailer.sent(['***@gmail.com','***@qq.com'], 'test fail')
 
 class AgideoBuildMailer
   def self.deliver_build_report(build, recipients, from, subject, message, sent_at = Time.now)
     self.send(recipients, subject)
   end
 
-  def self.send(recipients, subject)
+  def self.sent(recipients, subject)
     subject = CGI.escape(subject)
     cmd = "curl -s \"http://agi-notes.heroku.com/send_mail?t=#{recipients.join(",")}&s=#{subject}&b=#{subject}\""
     `#{cmd}`
