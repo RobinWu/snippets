4b0d6f9f0b88833c4ce9644b1eb605e565bc9056
RobinWu <robin.wu.cn@gmail.com>
2011-05-12 10:30:53 +0800

    add require_softwares sh

--- shell/install_softwares_on_ubuntu.sh
+++ shell/install_softwares_on_ubuntu.sh
@@ -24,8 +24,6 @@
 # sudo apt-get install curl dialog
 ### END REQUIREMENT SOFTWARE
 
-sudo apt-get install curl dialog -y
-
 DIALOG=${DIALOG=dialog}
 tempfile=`tempfile 2>/dev/null` || tempfile=/tmp/test$$
 trap "rm -f $tempfile" 0 1 2 5 15
