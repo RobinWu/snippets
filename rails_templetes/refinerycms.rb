f74b71e92399ed9153d81925b0160d9dfb63da33
robin <robin.wu.cn@gmail.com>
2012-09-17 13:17:10 +0800

    modified:   rails_templetes/refinerycms.rb

--- rails_templetes/refinerycms.rb
+++ rails_templetes/refinerycms.rb
@@ -4,12 +4,13 @@ VERSION_BAND = '2.0.0'
 gsub_file 'Gemfile', "gem 'jquery-rails'", "gem 'jquery-rails', '~> 2.0.0'"
 
 append_file 'Gemfile', <<-GEMFILE
-gem 'refinerycms', '~> #{VERSION_BAND}'
-gem 'refinerycms-i18n', '~> #{VERSION_BAND}'
-#  gem 'refinerycms-blog', '~> #{VERSION_BAND}'
-#  gem 'refinerycms-inquiries', '~> #{VERSION_BAND}'
-#  gem 'refinerycms-search', '~> #{VERSION_BAND}'
-#  gem 'refinerycms-page-images', '~> #{VERSION_BAND}'
+gem 'refinerycms', :git => 'git://github.com/refinery/refinerycms.git', :branch => '2-0-stable'
+gem 'refinerycms-i18n', :git => 'git://github.com/refinery/refinerycms-i18n.git', :branch => '2-0-stable'
+# gem 'refinerycms-i18n', '~> 2.0.0'
+# gem 'refinerycms-blog', '~> #{VERSION_BAND}'
+# gem 'refinerycms-inquiries', '~> #{VERSION_BAND}'
+# gem 'refinerycms-search', '~> #{VERSION_BAND}'
+# gem 'refinerycms-page-images', '~> #{VERSION_BAND}'
 
 gem 'execjs'
 gem 'therubyracer'
@@ -25,11 +26,12 @@ run 'bundle install'
 rake 'db:create'
 generate "refinery:cms --fresh-installation #{ARGV.join(' ')}"
 
-gsub_file 'config/initializers/refinery/i18n.rb', "", ""
 gsub_file 'config/initializers/refinery/i18n.rb', "# config.enabled = true", "config.enabled = true"
 gsub_file 'config/initializers/refinery/i18n.rb', "# config.default_locale = :en", "config.default_locale = 'zh-CN'"
 gsub_file 'config/initializers/refinery/i18n.rb', "# config.current_locale = :en", "config.current_locale = 'zh-CN'"
 gsub_file 'config/initializers/refinery/i18n.rb', "# config.default_frontend_locale = :en", "config.default_frontend_locale = 'zh-CN'"
+gsub_file 'config/initializers/refinery/i18n.rb', "# config.frontend_locales = [:en]", "config.frontend_locales = [:en, 'zh-CN']"
+# gsub_file 'config/initializers/refinery/i18n.rb', '# config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :"pt-BR"=>"Português", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"Simplified Chinese", :"zh-TW"=>"Traditional Chinese", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български"}', "config.locales = {:en=>'English', :'zh-CN'=>'Simplified Chinese'}"
 
 run 'git init'
 run 'git add .'
